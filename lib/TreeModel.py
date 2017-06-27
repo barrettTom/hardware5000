0e7a36cac145f58d05d2bab1e683b8baec906149
Tom Barrett <tombarrett@siu.edu>
2017-06-27 03:42:07 -0500

    -reorganized functions to make more sense

--- lib/TreeModel.py
+++ lib/TreeModel.py
@@ -3,7 +3,6 @@ from PyQt5.QtGui import QBrush, QColor
 from ast import literal_eval
 import xml.etree.ElementTree as ET
 
-from lib.xmllib import find
 from lib.TreeItem import TreeItem
 
 class TreeModel(QAbstractItemModel):
@@ -14,73 +13,47 @@ class TreeModel(QAbstractItemModel):
 
         self.setupModel(path)
 
-    def columnCount(self, parent):
-        if parent.isValid():
-            return parent.internalPointer().columnCount()
-        else:
-            return self.rootItem.columnCount()
+    def save(self,path=None):
+        if path is None:
+            path = self.path
 
-    def data(self, index, role):
-        if not index.isValid():
-            return None
-
-        item = index.internalPointer()
-
-        if role == Qt.BackgroundRole:
-            if item.depth == 0:
-                return QBrush(QColor(157,159,85))
-            elif item.depth == 1:
-                return QBrush(QColor(85,157,159))
-            elif item.depth == 2:
-                return QBrush(QColor(85,120,159))
-            elif item.depth == 3:
-                return QBrush(QColor(159,85,120))
-            elif item.depth == 4:
-                return QBrush(QColor(120,159,85))
-
-        if role == Qt.EditRole:
-            return item.data(index.column())
-
-        if role != Qt.DisplayRole:
-            return None
-
-        return item.data(index.column())
-
-    def flags(self, index):
-        if not index.isValid():
-            return Qt.NoItemFlags
-
-        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
-
-    def setData(self, index, value, role):
+        self.resetChanged()
         try:
-            XMLRoot = self.XMLTree.getroot()
-            item = index.internalPointer()
-
-            element = find(XMLRoot, item.data(0), item.data(1), item.data(2))
-
-            if index.column() == 0:
-                element.tag = value
-            elif index.column() == 1:
-                element.text = value
-            elif index.column() == 2:
-                for key in list(element.attrib):
-                    del element.attrib[key]
-                dValue = literal_eval(value)
-                for key in dValue:
-                    element.set(key,dValue[key])
-
-            item.setData(value,index.column())
-            return True
-
+            self.XMLTree.write(path)
         except:
-            return False
+            print("save error")
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.rootItem.data(section)
 
         return None
+
+    def rowCount(self, parent):
+        if parent.column() > 0:
+            return 0
+
+        if not parent.isValid():
+            parentItem = self.rootItem
+        else:
+            parentItem = parent.internalPointer()
+
+        return parentItem.childCount()
+
+    def columnCount(self, parent):
+        if parent.isValid():
+            return parent.internalPointer().columnCount()
+        else:
+            return self.rootItem.columnCount()
+
+    def flags(self, index):
+        if not index.isValid():
+            return Qt.NoItemFlags
+
+        if index.column() == 1 or index.column() == 3:
+            return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
+        else:
+            return Qt.ItemIsEnabled | Qt.ItemIsSelectable
 
     def index(self, row, column, parent):
         if not self.hasIndex(row, column, parent):
@@ -109,16 +82,63 @@ class TreeModel(QAbstractItemModel):
 
         return self.createIndex(parentItem.row(), 0, parentItem)
 
-    def rowCount(self, parent):
-        if parent.column() > 0:
-            return 0
+    def data(self, index, role):
+        if not index.isValid():
+            return None
 
-        if not parent.isValid():
-            parentItem = self.rootItem
-        else:
-            parentItem = parent.internalPointer()
+        item = index.internalPointer()
 
-        return parentItem.childCount()
+        if role == Qt.BackgroundRole:
+            if item.depth == 0:
+                return QBrush(QColor(157,159,85))
+            elif item.depth == 1:
+                return QBrush(QColor(85,157,159))
+            elif item.depth == 2:
+                return QBrush(QColor(85,120,159))
+            elif item.depth == 3:
+                return QBrush(QColor(159,85,120))
+            elif item.depth == 4:
+                return QBrush(QColor(120,159,85))
+
+        if role == Qt.EditRole:
+            return item.data(index.column())
+
+        if role != Qt.DisplayRole:
+            return None
+
+        return item.data(index.column())
+
+    def setData(self, index, value, role):
+        try:
+            XMLRoot = self.XMLTree.getroot()
+            item = index.internalPointer()
+
+            element = find(XMLRoot, item.data(0), item.data(1), item.data(2))
+
+            if index.column() == 0:
+                element.tag = value
+            elif index.column() == 1:
+                element.text = value
+            elif index.column() == 2:
+                for key in list(element.attrib):
+                    del element.attrib[key]
+                dValue = literal_eval(value)
+                for key in dValue:
+                    element.set(key,dValue[key])
+
+            item.setData(value,index.column())
+            return True
+
+        except:
+            return False
+
+    def find(self, root, tag, text, attrib):
+        for element in root.iter(tag):
+            tryText = str(element.text)
+            if tryText == "None":
+                tryText = ""
+            if tryText == text and str(element.attrib) == atrrib:
+                return element
 
     def setupModel(self, path):
         self.tree = ET.parse(path)
@@ -171,50 +191,6 @@ class TreeModel(QAbstractItemModel):
 
         return modules
 
-    def treeHardware(self, parent, puts):
-        for put in puts:
-            if len(put['hardware'].split(".")) == 2:
-                childItem = TreeItem((put['hardware'], put['hcomment'], put['parameter'], put['pcomment']), parent, 3)
-                parent.appendChild(childItem)
-
-        for put in puts:
-            pModule = put['hardware'].split(".")
-            if len(pModule) == 3:
-                pModule = ".".join(pModule[0:2])
-
-                root = self.base
-                hwCount = root.childCount()
-                for i in range(hwCount):
-
-                    hardware = root.child(i)
-                    childCount = hardware.childCount()
-                    for j in range(childCount):
-
-                        p = hardware.child(j)
-                        moduleCount = p.childCount()
-                        for k in range(moduleCount):
-
-                            module = p.child(k)
-                            if module.data(0) ==  pModule:
-                                childItem = TreeItem((put['hardware'], put['hcomment'], put['parameter'], put['pcomment']), module, 4)
-                                module.appendChild(childItem)
-
-    def draw(self, modules):
-        for module in modules:
-            if len(module['inputs']) or len(module['outputs']):
-                item = TreeItem((module['hardware'], module['hcomment'],"",""), self.base, 1)
-                self.base.appendChild(item)
-
-                inputs = TreeItem(("Inputs","","",""), item, 2)
-                item.appendChild(inputs)
-                self.treeHardware(inputs, module['inputs'])
-
-                outputs = TreeItem(("Outputs","","",""), item, 2)
-                item.appendChild(outputs)
-                self.treeHardware(outputs, module['outputs'])
-
-        #for i in range(4):
-        #    self.TreeWidget.resizeColumnToContents(i)
 
     def getParameterComment(self, comment):
         s = comment['parameter'].split('.')
@@ -258,27 +234,45 @@ class TreeModel(QAbstractItemModel):
         comment['parameter'] = ""
         return comment
 
-    def setColor(self, color, node=None):
-        if node is None:
-            node = self.rootItem
-        for i in range(node.childCount()):
-            node.child(i).setColorState(color)
-            self.setColor(color, node.child(i))
+    def draw(self, modules):
+        for module in modules:
+            if len(module['inputs']) or len(module['outputs']):
+                item = TreeItem((module['hardware'], module['hcomment'],"",""), self.base, 1)
+                self.base.appendChild(item)
 
-    def save(self,path=None):
-        if path is None:
-            path = self.path
+                inputs = TreeItem(("Inputs","","",""), item, 2)
+                item.appendChild(inputs)
+                self.treeHardware(inputs, module['inputs'])
 
-        self.resetChanged()
-        try:
-            self.XMLTree.write(path)
-        except:
-            print("save error")
+                outputs = TreeItem(("Outputs","","",""), item, 2)
+                item.appendChild(outputs)
+                self.treeHardware(outputs, module['outputs'])
 
-    def find(self, root, tag, text, attrib):
-        for element in root.iter(tag):
-            tryText = str(element.text)
-            if tryText == "None":
-                tryText = ""
-            if tryText == text and str(element.attrib) == atrrib:
-                return element
+    def treeHardware(self, parent, puts):
+        for put in puts:
+            if len(put['hardware'].split(".")) == 2:
+                childItem = TreeItem((put['hardware'], put['hcomment'], put['parameter'], put['pcomment']), parent, 3)
+                parent.appendChild(childItem)
+
+        for put in puts:
+            pModule = put['hardware'].split(".")
+            if len(pModule) == 3:
+                pModule = ".".join(pModule[0:2])
+
+                root = self.base
+                hwCount = root.childCount()
+                for i in range(hwCount):
+
+                    hardware = root.child(i)
+                    childCount = hardware.childCount()
+                    for j in range(childCount):
+
+                        p = hardware.child(j)
+                        moduleCount = p.childCount()
+                        for k in range(moduleCount):
+
+                            module = p.child(k)
+                            if module.data(0) ==  pModule:
+                                childItem = TreeItem((put['hardware'], put['hcomment'], put['parameter'], put['pcomment']), module, 4)
+                                module.appendChild(childItem)
+
